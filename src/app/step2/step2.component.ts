44e4d00e240861a13850b621794d7c63aa1f712e
steve-237 <steve0peg@gmail.com>
2025-02-22 22:54:30 +0100

    add step 2 component

---
+++ src/app/step2/step2.component.ts
@@ -0,0 +1,12 @@
+import { Component, inject } from '@angular/core';
+import { ConfiguratorService } from '../configurator.service';
+
+@Component({
+  selector: 'app-step2',
+  imports: [],
+  templateUrl: './step2.component.html',
+  styleUrl: './step2.component.scss'
+})
+export class Step2Component {
+  service = inject(ConfiguratorService);
+}
