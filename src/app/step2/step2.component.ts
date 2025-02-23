ae3ad9ff2bd56eaf52f2c52473ff38748eee5f9a
steve-237 <steve0peg@gmail.com>
2025-02-23 23:54:40 +0100

    add step 2 content

--- src/app/step2/step2.component.ts
+++ src/app/step2/step2.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CarService } from '../services/car.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-step2',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './step2.component.html',
   styleUrl: './step2.component.scss'
 })
